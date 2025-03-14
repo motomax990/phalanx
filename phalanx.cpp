15e9ce7d2055968ce560efe06fa25f46e4e735a8
motomax990 <motomax990@gmail.com>
2025-03-14 22:56:51 +0300

    Реализована проверка возможности хода

--- phalanx.cpp
+++ phalanx.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 enum EDir{
-    UP,
-    DW ,
-    LF,
-    RT,
-    UPRT,
-    UPLT,
-    DWRT,
-    DWLT
+    UP=0,
+    DW=1,
+    LT=2,
+    RT=3,
+    UPRT=4,
+    UPLT=5,
+    DWRT=6,
+    DWLT=7
 };
 
 //Размеры поля 12x14
@@ -56,8 +56,186 @@ int checkWin(vector < vector<int> > &mtr){
     return 0;
 }
 
-bool checkMoveAbility(vector < vector<int> > &mtr,int x1,int y1,int x2, int y2, int len){
-    if()
+bool checkMoveAbility(vector < vector<int> > &mtr,int crp, EDir dir, int bx1,int by1,int bx2, int by2, int e1, int e2, int len){
+    if (e1<0 or e2 <0 or e1 > CPH or e2 > CPW) return false;
+    if (dir == EDir::UP) {
+        if (bx1-e1 > len) return false;
+        for (size_t i = bx1-1; i > e1; i--)
+        {
+            if (mtr[i][e2] != 0) return false;
+        }
+        if (mtr[e1][e2] == crp) return false;
+        else if (mtr[e1][e2] == 0) {
+            return true;
+        }
+        else {
+            int drp = 3 - crp;
+            int cr1 = e1;
+            int crL = 0;
+            while (cr1 >= 0 and mtr[cr1][e2] == drp) { crL++; cr1--; }
+            if (crL > len) return false;
+        }
+        return true;
+    }
+    else if (dir == EDir::DW) {
+        if (e1 - bx1 > len) return false;
+        for (size_t i = bx1+1; i < e1; i++)
+        {
+            if (mtr[i][e2] != 0) return false;
+        }
+        if (mtr[e1][e2] == crp) return false;
+        else if (mtr[e1][e2] == 0) {
+            return true;
+        }
+        else {
+            int drp = 3 - crp;
+            int cr1 = e1;
+            int crL = 0;
+            while (cr1 <= CPH and mtr[cr1][e2] == drp) {
+                crL++; cr1++;
+        }
+            if (crL > len) return false;
+        }
+        return true;
+    }
+    else if (dir == EDir::RT) {
+            if (e2 - by1 > len) return false;
+            for (size_t i = by1+1; i < e2; i++)
+            {
+                if (mtr[e1][i] != 0) return false;
+            }
+            if (mtr[e1][e2] == crp) return false;
+            else if (mtr[e1][e2] == 0) {
+                return true;
+            }
+            else {
+                int drp = 3 - crp;
+                int cr2 = e2;
+                int crL = 0;
+                while (cr2 <= CPW and mtr[e1][cr2] == drp) {
+                    crL++; cr2++;
+                }
+                if (crL > len) return false;
+            }
+            return true;
+    }
+    else if (dir == EDir::LT) {
+        if (by1 - e2 > len) return false;
+        for (size_t i = by1-1; i > e2; i--)
+        {
+            if (mtr[e1][i] != 0) return false;
+        }
+        if (mtr[e1][e2] == crp) return false;
+        else if (mtr[e1][e2] == 0) {
+            return true;
+        }
+        else {
+            int drp = 3 - crp;
+            int cr2 = e2;
+            int crL = 0;
+            while (cr2 >= 0 and mtr[e1][cr2] == drp) {
+                crL++; cr2--;
+            }
+            if (crL > len) return false;
+        }
+        return true;
+    }
+    else if (dir == EDir::UPRT) {
+        int cnt = 0;
+        for (size_t i = bx1 - 1, j = by1 + 1; i > e1 and j < e2; i--, j++)
+        {
+                if (mtr[i][j] != 0) return false;
+                cnt++;
+        }
+        if (cnt > len) return false;
+        if (mtr[e1][e2] == crp) return false;
+        else if (mtr[e1][e2] == 0) {
+            return true;
+        }
+        else {
+            int drp = 3 - crp;
+            int crx1 = e1;
+            int cry1 = e2;
+            int crL = 0;
+            while (crx1 >= 0 and cry1 < CPW and mtr[crx1][cry1] == drp) { crL++; crx1--; cry1++;}
+            if (crL > len) return false;
+        }
+        return true;
+    }
+    else if (dir == EDir::UPLT) {
+        int cnt = 0;
+        for (size_t i = bx1 - 1, j = by1 - 1; i > e1 and j > e2; i--, j--)
+        {
+            
+                if (mtr[i][j] != 0) return false;
+                cnt++;
+
+            
+        }
+        if (cnt > len) return false;
+        if (mtr[e1][e2] == crp) return false;
+        else if (mtr[e1][e2] == 0) {
+            return true;
+        }
+        else {
+            int drp = 3 - crp;
+            int crx1 = e1;
+            int cry1 = e2;
+            int crL = 0;
+            while (crx1 >= 0 and cry1 >= 0 and mtr[crx1][cry1] == drp) { crL++; crx1--; cry1--; }
+            if (crL > len) return false;
+        }
+        return true;
+    }
+    else if (dir == EDir::DWRT) {
+        int cnt = 0;
+        for (size_t i = bx1 + 1, j = by1 + 1; i < e1 and j < e2; i++,j++)
+        {
+            
+                if (mtr[i][j] != 0) return false;
+                cnt++;
+
+            
+        }
+        if (cnt > len) return false;
+        if (mtr[e1][e2] == crp) return false;
+        else if (mtr[e1][e2] == 0) {
+            return true;
+        }
+        else {
+            int drp = 3 - crp;
+            int crx1 = e1;
+            int cry1 = e2;
+            int crL = 0;
+            while (crx1 < CPH and cry1 < CPW and mtr[crx1][cry1] == drp) { crL++; crx1++; cry1++; }
+            if (crL > len) return false;
+        }
+        return true;
+        
+    }
+    else if (dir == EDir::DWLT) {
+        int cnt = 0;
+        for (size_t i = bx1 + 1, j = by1 - 1; i < e1 and  j > e2; i++, j--)
+        {
+                if (mtr[i][j] != 0) return false;
+                cnt++;
+
+        }
+        if (cnt > len) return false;
+        if (mtr[e1][e2] == crp) return false;
+        else if (mtr[e1][e2] == 0) {
+            return true;
+        }
+        else {
+            int drp = 3 - crp;
+            int crx1 = e1;
+            int cry1 = e2;
+            int crL = 0;
+            while (crx1 < CPH and cry1 >= 0 and mtr[crx1][cry1] == drp) { crL++; crx1++; cry1--; }
+            if (crL > len) return false;
+        }
+        return true;
+    }
     return false;
 }
 
@@ -67,19 +245,24 @@ int main(){
 	for (int cnt = 0; cnt < CPH; cnt++)
 	{
 		strLiter[cnt] = 'l'-cnt;
+        cout << strLiter[cnt] << " ";
 	}
+    cout << endl;
 	for (int cnt = 0; cnt < CPW; cnt++)
 	{
-		strLiter[cnt] = 'A'+cnt;
-	}
+        colLiter[cnt] = 'A'+cnt;
+        cout << colLiter[cnt] << " ";
+    }
+    cout << endl;
     buildBaseMtr(mtr);
     printMap(mtr);
-
+    mtr[4][6] = 1;
+    mtr[5][7] = 1;
 
 	return 0;
 }
 
 /*
- *
+ * 1, EDir::UPLT, b 9 6, b2 10 7, e 7 4, 2 
  *
  */
