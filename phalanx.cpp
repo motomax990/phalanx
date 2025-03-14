375bae53a302d599fbbbacacd6207351d9899d5f
motomax990 <motomax990@gmail.com>
2025-03-14 17:11:11 +0300

    first test

--- phalanx.cpp
+++ phalanx.cpp
@@ -1,9 +1,25 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
+//Размеры поля 12x14
+const int64_t CPH = 12;
+const int64_t CPW = 14;
+//Удобный перевод координат для вывода
+char strLiter[CPH];
+char colLiter[CPW];
 
+int main(){
+	for (int cnt = 0; cnt < CPH; cnt++)
+	{
+		strLiter[cnt] = 'l'-cnt;
+	}
+	for (int cnt = 0; cnt < CPW; cnt++)
+	{
+		strLiter[cnt] = 'A'+cnt;
+	}
 
-
-int main()
-{
-    std::cout << "Hello World!\n";
+	return 0;
 }
